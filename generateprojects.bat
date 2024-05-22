1d9da700d7aacfc87094e94989517ee69203ecf6
Nikolay Natov <64912680+NikiNatov@users.noreply.github.com>
2024-05-22 22:46:58 +0300

    Added project generation scripts for Visual Studio 2022

---
+++ generateprojects.bat
@@ -0,0 +1,3 @@
+@echo off
+call extern\premake\premake5.exe vs2022
+PAUSE
---
+++ premake5.lua
@@ -0,0 +1,53 @@
+workspace "hexray-gpu"
+	architecture "x64"
+	startproject "hexray-gpu"
+
+	configurations 
+	{
+		"Debug",
+		"Release"
+	}
+
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
+	outputdir = "%{cfg.buildcfg}"
+
+project "hexray-gpu"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	systemversion "latest"
+	staticruntime "on"
+	characterset("ASCII")
+
+	targetdir("%{wks.location}/bin/" .. outputdir)
+	objdir("%{wks.location}/tmp/" .. outputdir)
+
+	files
+	{
+		"%{wks.location}/src/**.cpp",
+		"%{wks.location}/src/**.h"
+	}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+		defines
+		{
+			"HEXRAY_DEBUG",
+			"_DEBUG"
+		}
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+		defines
+		{
+			"HEXRAY_RELEASE",
+			"NDEBUG"
+		}
