b81e2151fcf5cf07391a730fcc73d2cb9305a22b
vkaytsanov <vkaytsanov@yahoo.com>
2024-06-21 23:17:28 +0300

    Convert the degrees to radians as glm::rotate expects radians

--- src/scene/component.h
+++ src/scene/component.h
@@ -56,9 +56,9 @@ struct TransformComponent
 	{
 		glm::mat4 translation = glm::translate(glm::mat4(1.0f), Translation);
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), Scale);
-		glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), Rotation.z, { 0.0f, 0.0f, 1.0f }) *
-			glm::rotate(glm::mat4(1.0f), Rotation.y, { 0.0f, 1.0f, 0.0f }) *
-			glm::rotate(glm::mat4(1.0f), Rotation.x, { 1.0f, 0.0f, 0.0f });
+		glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), glm::radians(Rotation.z), {0.0f, 0.0f, 1.0f}) *
+			glm::rotate(glm::mat4(1.0f), glm::radians(Rotation.y), { 0.0f, 1.0f, 0.0f }) *
+			glm::rotate(glm::mat4(1.0f), glm::radians(Rotation.x), { 1.0f, 0.0f, 0.0f });
 
 		return translation * rotation * scale;
 	}
