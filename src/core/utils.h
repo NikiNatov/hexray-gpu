d7fabbbcb29c491b7bca179c5d571ea53f65ec59
vkaytsanov <vkaytsanov@yahoo.com>
2024-06-30 00:23:35 +0300

    Add missed utils header

--- src/core/utils.h
+++ src/core/utils.h
@@ -11,6 +11,8 @@ std::string ToString(const std::wstring& str);
 std::wstring ToWString(const std::string& str);
 std::vector<std::string> Tokenize(const std::string& str);
 
+bool AreAllBytesZero(void* data, size_t size);
+
 /// a simple RAII class for FILE* pointers.
 class FileRAII
 {
@@ -20,4 +22,23 @@ public:
 	~FileRAII() { if (held) fclose(held); held = NULL; }
 	FileRAII(const FileRAII&) = delete;
 	FileRAII& operator = (const FileRAII&) = delete;
+};
+
+// Based on boost::hash_combine
+template <typename T>
+void HashCombine(size_t& accumulatedHash, const T& value)
+{
+	accumulatedHash ^= std::hash<T>()(value) + 0x9e3779b9 + (accumulatedHash << 6) + (accumulatedHash >> 2);
+}
+
+template <typename T, typename U>
+struct std::hash<std::pair<T, U>>
+{
+	std::size_t operator()(const std::pair<T, U>& p) const noexcept
+	{
+		std::size_t hash = 1337;
+		HashCombine(hash, p.first);
+		HashCombine(hash, p.second);
+		return hash;
+	}
 };
